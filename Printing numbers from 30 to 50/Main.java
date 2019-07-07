46e800e3c1680332338a7cc644b40dd03097314d
Dhananjay Sinha <46321708+Jitukaman@users.noreply.github.com>
2019-07-07 08:24:07 +0530

    Test code pushed to repository.

---
+++ Printing numbers from 30 to 50/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+	public static void main (String[] args){
+	     Scanner in=new Scanner(System.in);
+      int n=30;
+      int l=50;
+      int i;
+    
+       for(i=n;i<=l;i++)
+       {
+        System.out.println(i);
+	   }
+   }
+}
